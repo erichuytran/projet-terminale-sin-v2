f479d61814200ee905772ad47eb2809c4644983d
erictran87 <erictran@live.fr>
2017-06-04 17:05:42 +0200

    first commit

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var session = require('express-session')
 
 var app = express();
+
+function checkAuth (req, res, next) {
+	console.log('CHECKAuth ' + req.url);
+	// don't serve /secure to those not logged in
+	// you should add to this list, for each and every secure url
+	console.log("CHECKAuth session : ", req.session);
+	if ((req.url === '/controle' || req.url === '/trainconfig') && (!req.session || !req.session.authenticated)) {
+		//res.render('unauthorised', { status: 403 });
+		req.flash('warning', 'Pour accéder à cette page, merci de vous identifier.');
+        res.redirect('/');
+		return;
+	}
+	next();
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +33,21 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+app.use(require('flash')());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(checkAuth);
+
 
 app.use('/', routes);
 app.use('/users', users);
@@ -31,7 +58,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
 // error handlers
 
 // development error handler
@@ -55,6 +81,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var SerialPort = require('serialport');
+
+// On ouvre une nouvelle liaison série
+// '/dev/tty-usbserial1' à modifier selon la carte arduino
+var port = new SerialPort('/dev/ttyACM0', {
+  baudRate: 9600 //vitesse de transmission de la liason série (voir sketch Arduino)
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-      res.render('index', { title: 'La Loco', values: req.body });
+      res.render('login');
+      req.session.flash = [];
 });
+
+router.post('/login', function(req, res, next) {
+    // you might like to do a database look-up or something more scalable here
+
+    if (req.body.username && req.body.username === 'user' && req.body.password && req.body.password === 'pass') {
+        console.log("AUTH OK");
+        req.session.authenticated = true;
+        console.log('auth session: ', req.session);
+        res.redirect('/trainconfig');
+    } else {
+        console.log("AUTH KO");
+        req.flash('danger', 'Utilisateur et/ou mot de passe incorrect(s)');
+        res.redirect('/');
+    }
+});
+
+router.get('/logout', function(req, res, next) {
+		delete req.session.authenticated;
+    req.flash('info', 'Vous êtes déconnecté.');
+		res.redirect('/');
+});
+
+router.get('/trainconfig', function(req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    user: req.session
+  });
+});
+
 
 router.post('/control', function(req, res, next) {
     console.log(JSON.stringify(req.body));
-    // Check if request is AJAX / XHR
-    var isAjaxRequest = req.xhr;
-    if (isAjaxRequest) {
-        res.json(req.body);
-    }
-    else {
-        // If request is synchrone POST, send HTML render
-        res.render('index', { title: 'La Loco', values: req.body });
-    }
+    var dataTrains = JSON.parse(req.body);
 
+    port.write(dataTrains, function(){
+      console.log('Train A : ' + 'Vitesse = ' + dataTrains.speed0 + 'Light = ' + dataTrains.speed0 +
+      ' - Train B :' + 'Vitesse = ' + dataTrains.speed1 + 'Light = ' + dataTrains.speed1);
+    });
 });
 
 module.exports = router;
