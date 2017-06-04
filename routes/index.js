2cd345d9b058efd4770782aecc632f4fb724d5ee
erictran87 <erictran@live.fr>
2017-06-04 21:51:13 +0200

    added navBar

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var SerialPort = require('serialport');
+var SerialPort = require("serialport");
 
 // On ouvre une nouvelle liaison série
 // '/dev/tty-usbserial1' à modifier selon la carte arduino
+
 var port = new SerialPort('/dev/ttyACM0', {
+  autoOpen: false,
   baudRate: 9600 //vitesse de transmission de la liason série (voir sketch Arduino)
+});
+
+port.open(function(err){
+  if (err) {
+    throw err;
+    //simuArduino.open
+  }
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-      res.render('login');
+      res.render('login', {title: 'Le Tchouchou'});
       req.session.flash = [];
 });
 
@@ -36,19 +45,24 @@ router.get('/logout', function(req, res, next) {
 
 router.get('/trainconfig', function(req, res, next) {
   res.render('index', {
-    title: 'Express',
+    title: 'Le Tchouchou',
     user: req.session
   });
 });
 
 
 router.post('/control', function(req, res, next) {
-    var dataTrains = JSON.parse(req.body);
+    var dataTrains = JSON.stringify(req.body);
 
     port.write(dataTrains, function(){
-      console.log('Train A : ' + 'Vitesse = ' + dataTrains.speed0 + 'Light = ' + dataTrains.speed0 +
-      ' - Train B :' + 'Vitesse = ' + dataTrains.speed1 + 'Light = ' + dataTrains.speed1);
+      console.log('Train A : ' + 'Vitesse = ' + req.body.speed0 + 'Light = ' + req.body.light0 +
+      ' - Train B :' + 'Vitesse = ' + req.body.speed1 + 'Light = ' + req.body.light1);
     });
+
+    // console.log('Train A : ' + 'Vitesse = ' + req.body.speed0 + ' Light = ' + req.body.light0 +
+    // ' - Train B :' + ' Vitesse = ' + req.body.speed1 + ' Light = ' + req.body.light1);
+
+    res.json("ok");
 });
 
 module.exports = router;
